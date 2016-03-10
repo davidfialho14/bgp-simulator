5debf4c75b55b17d5cd29352bccf273b75bf5693
David Fialho <fialho.david@gmail.com>
2016-03-10 11:47:14 +0000

    refactored the route table tests and added a new test for setting an attribute for a destination and neighbor that already exist

--- test/network/RouteTableTest.java
+++ test/network/RouteTableTest.java
@@ -1,26 +1,44 @@
 package network;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 public class RouteTableTest {
 
+    private RouteTable table;
+    private Node defaultNeighbour;
+
+    @Before
+    public void setUp() throws Exception {
+        defaultNeighbour = new Node(null, 0, null);
+        table = new RouteTable(Collections.singletonList(defaultNeighbour), null);
+    }
+
     @Test
     public void setAttributeForNonExistingDestination_AttributeWasSetToPairDestinationNeighbour() throws Exception {
-        Node neighbour = new Node(null, 0, null);
-        Node[] neighbours = { neighbour };
-        RouteTable table = new RouteTable(Arrays.asList(neighbours), null);
         Node destination = new Node(null, 1, null); // any destination node
 
         Attribute expectedAttribute = ComponentFactory.createAttribute(0);
-        Attribute attribute = ComponentFactory.createAttribute(0);
-        table.setAttribute(destination, neighbour, attribute);
+        table.setAttribute(destination, defaultNeighbour, ComponentFactory.createAttribute(0));
 
-        assertThat(table.getAttribute(destination, neighbour), equalTo(expectedAttribute));
+        assertThat(table.getAttribute(destination, defaultNeighbour), equalTo(expectedAttribute));
+    }
+
+    @Test
+    public void setAttributeForExistingDestination_AttributeChanged() throws Exception {
+        Node destination = new Node(null, 1, null); // any destination node
+        table.setAttribute(destination, defaultNeighbour, ComponentFactory.createAttribute(0));
+
+        Attribute expectedAttribute = ComponentFactory.createAttribute(1);
+        table.setAttribute(destination, defaultNeighbour, ComponentFactory.createAttribute(1));
+
+        assertThat(table.getAttribute(destination, defaultNeighbour), equalTo(expectedAttribute));
     }
 
     // set attribute for destination that does not exist (neighbour exists)
