a398cd321984858817065cbb53e35083afac350f
David Fialho <fialho.david@gmail.com>
2016-03-11 22:23:08 +0000

    added new test for the Node.learn method

--- test/network/NodeTest.java
+++ test/network/NodeTest.java
@@ -7,14 +7,17 @@ import org.mockito.MockitoAnnotations;
 import policies.DummyAttribute;
 import policies.DummyAttributeFactory;
 import policies.DummyLabel;
-import protocols.DummyProtocolFactory;
+import protocols.DummyProtocol;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class NodeTest {
 
     @Mock
     Network mockNetwork;
+    @Mock
+    DummyProtocol stubProtocol;
 
     @Before
     public void setUp() throws Exception {
@@ -22,28 +25,50 @@ public class NodeTest {
         when(mockNetwork.getAttrFactory()).thenReturn(new DummyAttributeFactory());
     }
 
-    private Node createNodeWithEmptyRouteTable(Network network, ProtocolFactory protocolFactory) {
-        Node node = new Node(network, 0, protocolFactory.createProtocol(0));
+    private Node createNodeWithEmptyRouteTable(Network network) {
+        Node node = new Node(network, 0, this.stubProtocol);
         node.routeTable = new RouteTable(node.getOutNeighbours(), network.getAttrFactory());
         return node;
     }
 
     @Test
     public void
-    learn_NodeLinkedBothWaysWithANeighbourAndWithEmptyTableLearnsValidRoute_ExportsValidRoute() throws Exception {
-        DummyProtocolFactory protocolFactory = new DummyProtocolFactory();
-        Node node = createNodeWithEmptyRouteTable(mockNetwork, protocolFactory);
-        Node exportingNode = new Node(mockNetwork, 1, protocolFactory.createProtocol(1));
+    learn_NodeLinkedBothWaysWithANeighbourAndWithEmptyTableLearnsValidRoute_ExportsExtendedValidRoute()
+            throws Exception {
+        Node node = createNodeWithEmptyRouteTable(mockNetwork);
+        Node exportingNode = new Node(mockNetwork, 1, stubProtocol);
         Link outLink = new Link(node, exportingNode, new DummyLabel());
         node.addOutLink(outLink);
         Link inLink = new Link(exportingNode, node, new DummyLabel());
         node.addInLink(inLink);
         Route learnedRoute = new Route(exportingNode, new DummyAttribute(), new PathAttribute());
+        when(stubProtocol.extend(outLink, learnedRoute.getAttribute())).thenReturn(new DummyAttribute());
 
         Route expectedRoute = new Route(exportingNode, new DummyAttribute(), new PathAttribute(exportingNode));
         node.learn(outLink, learnedRoute);
 
         verify(mockNetwork).export(inLink, expectedRoute);
     }
+
+    @Test
+    public void
+    learn_NodeLinkedBothWaysWithANeighbourAndWithEmptyTableLearnsInValidRoute_ExportsInvalidRoute() throws Exception {
+        Node node = createNodeWithEmptyRouteTable(mockNetwork);
+        Node exportingNode = new Node(mockNetwork, 1, stubProtocol);
+        Link outLink = new Link(node, exportingNode, new DummyLabel());
+        node.addOutLink(outLink);
+        Link inLink = new Link(exportingNode, node, new DummyLabel());
+        node.addInLink(inLink);
+        DummyAttributeFactory attributeFactory = new DummyAttributeFactory();
+        Route learnedRoute = Route.createInvalid(exportingNode, attributeFactory);
+        when(stubProtocol.extend(outLink, learnedRoute.getAttribute())).thenReturn(attributeFactory.createInvalid());
+
+        Route expectedRoute = Route.createInvalid(exportingNode, attributeFactory);
+        node.learn(outLink, learnedRoute);
+
+        verify(mockNetwork).export(inLink, expectedRoute);
+    }
+
+    // TODO
 
 }
