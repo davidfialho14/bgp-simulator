2808fb72be7a8d02d74d1c21007a3095809214c1
David Fialho <fialho.david@gmail.com>
2016-03-31 09:59:01 +0100

    fixed compile errors for the test Factory class

--- test/network/Factory.java
+++ test/network/Factory.java
@@ -2,13 +2,69 @@ package network;
 
 import policies.DummyAttribute;
 import policies.DummyLabel;
-import protocols.DummyProtocolFactory;
 
 /**
  * Factory class to create instances necessary for abstract and interface classes necessary to build a network.
  * The created elements are guaranteed to be able to work together.
  */
 public class Factory {
+
+    /**
+     * Creates a node with a fixed Id. All nodes created with this method will have the same id. All the other node
+     * attributes will be initialized to null.
+     * @return new node instance.
+     */
+    public static Node createNode() {
+        return new Node(null, 0);
+    }
+
+    /**
+     * Creates a node with the specified id. All the other node attributes will be initialized to null.
+     * @return new node instance.
+     */
+    public static Node createNode(int id) {
+        return new Node(null, id);
+    }
+
+    /**
+     * Creates n node instances with different ids. The returned nodes are ordered by id from the smaller id to the
+     * higher. All the other node attributes will be initialized to null.
+     * @return array with n node instances.
+     */
+    public static Node[] createNodes(int n) {
+        Node[] nodes = new Node[n];
+        for (int i = 0; i < n; i++) {
+            nodes[i] = new Node(null, i);
+        }
+
+        return nodes;
+    }
+
+    // used to create random nodes with different ids
+    private static int nodeId = 0;
+
+    /**
+     * Creates a node with random id. The ids will never repeat between calls. All the other node attributes will be
+     * initialized to null.
+     * @return new node instance.
+     */
+    public static Node createRandomNode() {
+        return new Node(null, nodeId++);
+    }
+
+    /**
+     * Creates n nodes with random ids. The ids will never repeat between calls. All the other node attributes will be
+     * initialized to null.
+     * @return array with the created nodes.
+     */
+    public static Node[] createRandomNodes(int n) {
+        Node[] nodes = new Node[n];
+        for (int i = 0; i < n; i++) {
+            nodes[i] = new Node(null, nodeId++);
+        }
+
+        return nodes;
+    }
 
     /**
      * Creates a link between two nodes of a network with dummy label. It creates the nodes from the ids.
@@ -19,11 +75,9 @@ public class Factory {
      * @return link instance.
      */
     public static Link createLink(Network network, int srcId, int destId) {
-        ProtocolFactory factory = new DummyProtocolFactory();
-
         // do not care about the link length
-        return new Link(new Node(network, srcId, factory.createProtocol(srcId)),
-                new Node(network, destId, factory.createProtocol(destId)), null);
+        return new Link(new Node(network, srcId),
+                new Node(network, destId), null);
     }
 
     /**
@@ -34,74 +88,6 @@ public class Factory {
      */
     public static Link createRandomLink() {
         return new Link(createRandomNode(), createRandomNode(), new DummyLabel());
-    }
-
-    /**
-     * Creates a node instance with an arbitrary id. The network and stubProtocol are initialized with null.
-     * To be used when the id of node is not important and the network and stubProtocol are not used.
-     * @return new node instance.
-     */
-    public static Node createNode() {
-        return new Node(null, 0, null);
-    }
-
-    /**
-     * Creates a node instance with the specified id. The network and stubProtocol are initialized with null.
-     * To be used when the id of node is important and the network and stubProtocol are not used.
-     * @return new node instance.
-     */
-    public static Node createNode(int id) {
-        return new Node(null, id, null);
-    }
-
-    /**
-     * Creates n node instances with different ids. The returned nodes are ordered by id from the smaller id to the
-     * higher. The network and stubProtocol are initialized with null. To be used when the ids of the nodes is important
-     * but the network and stubProtocol are not used.
-     * @return array with n node instances.
-     */
-    public static Node[] createNodes(int n) {
-        Node[] nodes = new Node[n];
-        for (int i = 0; i < n; i++) {
-            nodes[i] = new Node(null, i, null);
-        }
-
-        return nodes;
-    }
-
-    // used to create random nodes
-    private static int nodeId = 0;
-
-    /**
-     * Creates a node with random id. The ids will never repeat between calls.
-     * @return new node instance.
-     */
-    public static Node createRandomNode() {
-        return new Node(null, nodeId++, null);
-    }
-
-    /**
-     * Creates n nodes with random ids. The ids will never repeat between calls.
-     * @return array with the created nodes.
-     */
-    public static Node[] createRandomNode(int n) {
-        Node[] nodes = new Node[n];
-        for (int i = 0; i < n; i++) {
-            nodes[i] = new Node(null, nodeId++, null);
-        }
-
-        return nodes;
-    }
-
-    /**
-     * Creates n node instances with different random ids. The is no order in the returned nodes. The network and
-     * stubProtocol are initialized with null. To be used when the ids of the nodes is important but the network and
-     * stubProtocol are not used. This method only exists for clarity in the test code, it indicates explicitly that the
-     * nodes created are random.
-     * @return array with n node instances.
-     */
-    public static Node[] createRandomNodes(int n) {
-        return createNodes(n);
     }
 
     /**
