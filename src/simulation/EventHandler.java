7971d9cc5ea3bec20d6873c899dab69356d9fbdd
David Fialho <fialho.david@gmail.com>
2016-04-03 09:51:09 +0100

    made the event methods all public

--- src/simulation/EventHandler.java
+++ src/simulation/EventHandler.java
@@ -4,36 +4,37 @@ import network.Link;
 
 public abstract class EventHandler {
 
-    void onBeforeSimulate() {
+    public void onBeforeSimulate() {
         // by default do nothing
     }
 
-    void onAfterSimulate() {
+    public void onAfterSimulate() {
         // by default do nothing
     }
 
-    void onBeforeLearn(Link link, Route exportedRoute) {
+    public void onBeforeLearn(Link link, Route exportedRoute) {
         // by default do nothing
     }
 
-    void onAfterLearn(Link link, Route exportedRoute, Route learnedRoute) {
+    public void onAfterLearn(Link link, Route exportedRoute, Route learnedRoute) {
         // by default do nothing
     }
 
-    void onBeforeSelect(NodeStateInfo nodeStateInfo, Link link, Route exportedRoute, Route learnedRoute,
+   public  void onBeforeSelect(NodeStateInfo nodeStateInfo, Link link, Route exportedRoute, Route learnedRoute,
                         Attribute prevSelectedAttribute, PathAttribute prevSelectedPath) {
         // by default do nothing
     }
-    void onAfterSelect(NodeStateInfo nodeStateInfo, Link link, Route exportedRoute, Route learnedRoute,
+   public  void onAfterSelect(NodeStateInfo nodeStateInfo, Link link, Route exportedRoute, Route learnedRoute,
                        Attribute prevSelectedAttribute, PathAttribute prevSelectedPath, Route selectedRoute) {
         // by default do nothing
     }
 
-    void onBeforeExport(Link link, Route route, ScheduledRoute prevScheduledRoute) {
+    public void onBeforeExport(Link link, Route route, ScheduledRoute prevScheduledRoute) {
         // by default do nothing
     }
 
-    void onAfterExport(Link link, Route route, ScheduledRoute prevScheduledRoute, ScheduledRoute scheduledRoute) {
+    public void onAfterExport(Link link, Route route, ScheduledRoute prevScheduledRoute,
+                              ScheduledRoute scheduledRoute) {
         // by default do nothing
     }
 }
