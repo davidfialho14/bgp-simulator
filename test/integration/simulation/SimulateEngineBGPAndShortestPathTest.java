f6d54c7933b2919dc24a446cc5c1b8baf2c331e0
David Fialho <fialho.david@gmail.com>
2016-04-04 09:34:01 +0100

    renamed the tests for the BGP and ShortestPath policy

--- test/integration/simulation/SimulateEngineBGPAndShortestPathTest.java
+++ test/integration/simulation/SimulateEngineBGPAndShortestPathTest.java
@@ -1,23 +1,22 @@
 package simulation;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import simulation.implementations.handlers.DebugEventHandler;
-import simulation.implementations.policies.gaorexford.GaoRexfordAttributeFactory;
+import simulation.implementations.policies.shortestpath.ShortestPathAttributeFactory;
 import simulation.implementations.protocols.BGPProtocol;
 import simulation.implementations.schedulers.FIFOScheduler;
-import simulation.networks.gaorexford.Topology0;
-import simulation.networks.gaorexford.Topology1;
-import simulation.networks.gaorexford.Topology2;
+import simulation.networks.shortestpath.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class SimulateEngineBGPAndGaoRexfordTest extends SimulateEngineTest {
+public class SimulateEngineBGPAndShortestPathTest extends SimulateEngineTest {
 
     @Before
     public void setUp() throws Exception {
-        engine = new SimulateEngine(new BGPProtocol(), new GaoRexfordAttributeFactory(),
+        engine = new SimulateEngine(new BGPProtocol(), new ShortestPathAttributeFactory(),
                 new FIFOScheduler(), new DebugEventHandler());
     }
 
@@ -42,9 +41,28 @@ public class SimulateEngineBGPAndGaoRexfordTest extends SimulateEngineTest {
     @Test(timeout = 2000)
     public void simulate_Topology2_Converges() throws Exception {
         topology = new Topology2();
-        engine.simulate(topology.getNetwork());
+        engine.simulate(topology.getNetwork(), 0);
         printTables();
 
-        assertThat(engine.getRouteTables(), is(topology.getExpectedRouteTables()));
+        assertThat(engine.getRouteTables(), is(topology.getExpectedRouteTables(0)));
+    }
+
+    @Test(timeout = 2000)
+    @Ignore
+    public void simulate_Topology3_DoesNotConverge() throws Exception {
+        topology = new Topology3();
+        engine.simulate(topology.getNetwork(), 0);
+        printTables();
+
+        assertThat(engine.getRouteTables(), is(topology.getExpectedRouteTables(0)));
+    }
+
+    @Test//(timeout = 2000)
+    public void simulate_Topology4_Converges() throws Exception {
+        topology = new Topology4();
+        engine.simulate(topology.getNetwork(), 0);
+        printTables();
+
+        assertThat(engine.getRouteTables(), is(topology.getExpectedRouteTables(0)));
     }
 }
