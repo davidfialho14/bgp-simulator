c06a380c121c30bcc656945763fc18606bfa9872
David Fialho <fialho.david@gmail.com>
2016-04-05 08:48:28 +0100

    made the event method calls protected instead of public

--- src/simulation/implementations/handlers/DebugEventHandler.java
+++ src/simulation/implementations/handlers/DebugEventHandler.java
@@ -7,33 +7,33 @@ import simulation.*;
 public class DebugEventHandler extends EventHandler {
 
     @Override
-    public void onBeforeSimulate() {
+    protected void onBeforeSimulate() {
         super.onBeforeSimulate();
         System.out.println("BEFORE SIMULATE");
     }
 
     @Override
-    public void onAfterSimulate() {
+    protected void onAfterSimulate() {
         super.onAfterSimulate();
         System.out.println("AFTER SIMULATE");
     }
 
 //    @Override
-//    public void onBeforeLearn(Link link, Route exportedRoute) {
+//    protected void onBeforeLearn(Link link, Route exportedRoute) {
 //        super.onBeforeLearn(link, exportedRoute);
 //
 //        System.out.println("BEFORE LEARN:\t" + link + "\t|\t" + exportedRoute);
 //    }
 
     @Override
-    public void onAfterLearn(Link link, Route exportedRoute, Route learnedRoute) {
+    protected void onAfterLearn(Link link, Route exportedRoute, Route learnedRoute) {
         super.onAfterLearn(link, exportedRoute, learnedRoute);
 
         System.out.println("LEARN:\t" + link + "\t|\t" + exportedRoute + "\t|\t" + learnedRoute);
     }
 
 //    @Override
-//    public void onBeforeSelect(NodeStateInfo nodeStateInfo, Link link, Route exportedRoute, Route learnedRoute,
+//    protected void onBeforeSelect(NodeStateInfo nodeStateInfo, Link link, Route exportedRoute, Route learnedRoute,
 //                               Attribute prevSelectedAttribute, PathAttribute prevSelectedPath) {
 //        super.onBeforeSelect(nodeStateInfo, link, exportedRoute, learnedRoute, prevSelectedAttribute, prevSelectedPath);
 //
@@ -42,7 +42,7 @@ public class DebugEventHandler extends EventHandler {
 //    }
 
     @Override
-    public void onAfterSelect(NodeStateInfo nodeStateInfo, Link link, Route exportedRoute, Route learnedRoute,
+    protected void onAfterSelect(NodeStateInfo nodeStateInfo, Link link, Route exportedRoute, Route learnedRoute,
                               Attribute prevSelectedAttribute, PathAttribute prevSelectedPath, Route selectedRoute) {
         super.onAfterSelect(nodeStateInfo, link, exportedRoute, learnedRoute,
                 prevSelectedAttribute, prevSelectedPath, selectedRoute);
@@ -52,7 +52,7 @@ public class DebugEventHandler extends EventHandler {
     }
 
     @Override
-    public void onAfterExport(Link link, Route route, ScheduledRoute prevScheduledRoute,
+    protected void onAfterExport(Link link, Route route, ScheduledRoute prevScheduledRoute,
                               ScheduledRoute scheduledRoute) {
         super.onAfterExport(link, route, prevScheduledRoute, scheduledRoute);
 
@@ -60,14 +60,14 @@ public class DebugEventHandler extends EventHandler {
     }
 
     @Override
-    public void onDiscardRoute(Link link, Route route) {
+    protected void onDiscardRoute(Link link, Route route) {
         super.onDiscardRoute(link, route);
 
         System.out.println("DISCARDED:\t" + link + "\t|\t" + route);
     }
 
     @Override
-    public void onOscillationDetection(Link link, Route exportedRoute, Route learnedRoute, Route exclRoute) {
+    protected void onOscillationDetection(Link link, Route exportedRoute, Route learnedRoute, Route exclRoute) {
         super.onOscillationDetection(link, exportedRoute, learnedRoute, exclRoute);
 
         System.out.println("DETECTED:\t" + link + "\t|\t" + learnedRoute + "\t|\t" + exclRoute);
