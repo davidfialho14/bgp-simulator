11fe55097e01adc01627b0cf98fc24c43eabd43f
David Fialho <fialho.david@gmail.com>
2016-04-17 12:34:14 +0100

    adjusted the Gao Rexford test topologies

--- test/integration/simulation/networks/gaorexford/Topology1.java
+++ test/integration/simulation/networks/gaorexford/Topology1.java
@@ -3,9 +3,10 @@ package simulation.networks.gaorexford;
 import network.Node;
 import network.exceptions.NodeExistsException;
 import network.exceptions.NodeNotFoundException;
-import simulation.RouteTable;
 import policies.implementations.gaorexford.CustomerLabel;
 import policies.implementations.gaorexford.ProviderLabel;
+import simulation.RouteTable;
+import simulation.networks.RouteTablesGenerator;
 import simulation.networks.Topology;
 
 import java.util.Map;
@@ -13,16 +14,20 @@ import java.util.Map;
 import static policies.implementations.gaorexford.CustomerAttribute.customer;
 import static policies.implementations.gaorexford.ProviderAttribute.provider;
 
+/**
+ * Topology description:
+ *
+ *  digraph topology1 {
+ *      0 -> 1 [label=c];
+ *      1 -> 0 [label=p];
+ *      2 -> 1 [label=c];
+ *      1 -> 2 [label=p];
+ *  }
+ */
 public class Topology1 extends Topology {
 
     /**
-     * Creates the topology 0.
-     *  digraph topology0 {
-     *      0 -> 1 [label=c];
-     *      1 -> 0 [label=p];
-     *      2 -> 1 [label=c];
-     *      1 -> 2 [label=p];
-     *  }
+     * Constructs the network topology according to the topology description
      */
     public Topology1() throws NodeExistsException, NodeNotFoundException {
         network.addNode(0);
@@ -36,36 +41,36 @@ public class Topology1 extends Topology {
 
     @Override
     public Map<Node, RouteTable> getExpectedRouteTablesForBGP(Integer destId) {
-        GaoRexfordRouteTablesGenerator generator = new GaoRexfordRouteTablesGenerator(network, destId);
+        RouteTablesGenerator generator = new RouteTablesGenerator(network.getNodes(), destId);
 
         /* node 0 route table
-            |   |    1   |
+            |   |  1, C  |
             |:-:|:------:|
             | 1 | c, [1] |
             | 2 |    •   |
          */
-        generator.setRoute(0, 1, 1, customer(), new int[]{1});
-        generator.setInvalidRoute(0, 2, 1);
+        generator.setRoute(0, 1, new CustomerLabel(), 1, customer(), new int[]{1});
+        generator.setInvalidRoute(0, 1, new CustomerLabel(), 2);
 
         /* node 1 route table
-            |   |    0   |    2   |
+            |   |  0, P  |  2, P  |
             |:-:|:------:|:------:|
             | 0 | p, [0] |    •   |
             | 2 |    •   | p, [2] |
          */
-        generator.setRoute(1, 0, 0, provider(), new int[]{0});
-        generator.setInvalidRoute(1, 0, 2);
-        generator.setInvalidRoute(1, 2, 0);
-        generator.setRoute(1, 2, 2, provider(), new int[]{2});
+        generator.setRoute(1, 0, new ProviderLabel(), 0, provider(), new int[]{0});
+        generator.setInvalidRoute(1, 0, new ProviderLabel(), 2);
+        generator.setInvalidRoute(1, 2, new ProviderLabel(), 0);
+        generator.setRoute(1, 2, new ProviderLabel(), 2, provider(), new int[]{2});
 
         /* node 2 route table
-            |   |    1   |
+            |   |  1, C  |
             |:-:|:------:|
             | 0 |    •   |
             | 1 | c, [1] |
          */
-        generator.setInvalidRoute(2, 0, 1);
-        generator.setRoute(2, 1, 1, customer(), new int[]{1});
+        generator.setInvalidRoute(2, 1, new CustomerLabel(), 0);
+        generator.setRoute(2, 1, new CustomerLabel(), 1, customer(), new int[]{1});
 
         return generator.getTables();
     }
