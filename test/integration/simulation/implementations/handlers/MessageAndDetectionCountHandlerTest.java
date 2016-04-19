298d19d0a8d4afa5524c0a696f4c60a8f042b7f9
David Fialho <fialho.david@gmail.com>
2016-04-19 22:12:21 +0100

    refactored the tests for the handlers with wrappers

--- test/integration/simulation/implementations/handlers/MessageAndDetectionCountHandlerTest.java
+++ test/integration/simulation/implementations/handlers/MessageAndDetectionCountHandlerTest.java
@@ -1,16 +1,20 @@
 package simulation.implementations.handlers;
 
+import network.Network;
 import org.junit.Before;
 import org.junit.Test;
 import policies.implementations.shortestpath.ShortestPathPolicy;
 import protocols.implementations.BGPProtocol;
 import simulation.SimulateEngine;
 import simulation.implementations.schedulers.FIFOScheduler;
-import simulation.networks.Topology;
-import simulation.networks.shortestpath.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static wrappers.ShortestPathWrapper.label;
+import static wrappers.network.FromNodeElement.from;
+import static wrappers.network.LinkElement.link;
+import static wrappers.network.NetworkWrapper.network;
+import static wrappers.network.ToNodeElement.to;
 
 public class MessageAndDetectionCountHandlerTest {
 
@@ -25,16 +29,24 @@ public class MessageAndDetectionCountHandlerTest {
 
     @Test
     public void Topology0_MessageCountIs1() throws Exception {
-        Topology topology = new Topology0();
-        engine.simulate(topology.getNetwork());
+        Network network0 = network(
+                link(from(0), to(1), label(1))
+        );
+
+        engine.simulate(network0);
 
         assertThat(eventHandler.getMessageCount(), is(1));
     }
 
     @Test
     public void Topology1_MessageCountIs1() throws Exception {
-        Topology topology = new Topology1();
-        engine.simulate(topology.getNetwork());
+        Network network1 = network(
+                link(from(0), to(1), label(1)),
+                link(from(1), to(2), label(1)),
+                link(from(0), to(2), label(0))
+        );
+
+        engine.simulate(network1);
 
         assertThat(eventHandler.getMessageCount(), is(4));
     }
