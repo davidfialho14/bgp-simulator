eb6656ea114c3c1298a1690ed71c591e3725267a
David Fialho <fialho.david@gmail.com>
2016-04-19 20:57:26 +0100

    moved the wrapper methods for the shortest path policy to its own file

--- test/integration/simulation/SimulateEngineBGPAndShortestPathTest.java
+++ test/integration/simulation/SimulateEngineBGPAndShortestPathTest.java
@@ -5,27 +5,22 @@ import network.Node;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import policies.implementations.shortestpath.ShortestPathAttribute;
-import policies.implementations.shortestpath.ShortestPathLabel;
 import policies.implementations.shortestpath.ShortestPathPolicy;
 import protocols.implementations.BGPProtocol;
 import simulation.implementations.handlers.DebugEventHandler;
 import simulation.implementations.schedulers.FIFOScheduler;
-import wrappers.routetable.OutLinkElement;
-import wrappers.routetable.RouteElement;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static wrappers.PathWrapper.path;
+import static wrappers.ShortestPathWrapper.*;
 import static wrappers.network.FromNodeElement.from;
 import static wrappers.network.LinkElement.link;
 import static wrappers.network.NetworkWrapper.network;
 import static wrappers.network.ToNodeElement.to;
 import static wrappers.routetable.DestinationElement.destination;
-import static wrappers.routetable.OutLinkElement.outLink;
 import static wrappers.routetable.OutLinkElement.selfLink;
 import static wrappers.routetable.RouteElement.invalid;
-import static wrappers.routetable.RouteElement.route;
 import static wrappers.routetable.RouteTableWrapper.table;
 
 public class SimulateEngineBGPAndShortestPathTest extends SimulateEngineTest {
@@ -34,34 +29,6 @@ public class SimulateEngineBGPAndShortestPathTest extends SimulateEngineTest {
     public void setUp() throws Exception {
         engine = new SimulateEngine(new BGPProtocol(), new ShortestPathPolicy(),
                 new FIFOScheduler(), new DebugEventHandler());
-    }
-
-    /**
-     * Wrapper around the ShortestPathLabel constructor to improve readability.
-     *
-     * @param length length of the label.
-     * @return a new ShortestPathLabel instance assign the given length.
-     */
-    private static ShortestPathLabel label(int length) {
-        return new ShortestPathLabel(length);
-    }
-
-    /**
-     * Wrapper around the usual link wrapper for shortest path labels.
-     *
-     * @return a new link instance with a label with the given length.
-     */
-    private static OutLinkElement splink(int srcId, int destId, int length) {
-        return outLink(srcId, destId, new ShortestPathLabel(length));
-    }
-
-    /**
-     * Wrapper around the usual route wrapper for shortest path attributes.
-     *
-     * @return a new route instance with a attribute and path.
-     */
-    private static RouteElement sproute(int length, PathAttribute path) {
-        return route(new ShortestPathAttribute(length), path);
     }
 
     @Test(timeout = 2000)
@@ -113,7 +80,6 @@ public class SimulateEngineBGPAndShortestPathTest extends SimulateEngineTest {
     }
 
     @Test(timeout = 2000)
-    @Ignore
     public void simulate_Topology2_Converges() throws Exception {
         Network network2 = network(
                 link(from(0), to(1), label(1)),
