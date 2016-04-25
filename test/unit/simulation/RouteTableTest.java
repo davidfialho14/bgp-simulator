5c639f7cff1702825d7e17c8462c7ddb0aee77a0
David Fialho <fialho.david@gmail.com>
2016-04-25 23:11:03 +0100

    added 3 more tests for the setRoute() and getRoute() methods

--- test/unit/simulation/RouteTableTest.java
+++ test/unit/simulation/RouteTableTest.java
@@ -9,8 +9,10 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static simulation.Route.invalidRoute;
 import static wrappers.DummyWrapper.*;
+import static wrappers.DummyWrapper.dummyRoute;
 import static wrappers.PathWrapper.path;
 import static wrappers.network.NetworkWrapper.anyNode;
+import static wrappers.routetable.DestinationElement.destination;
 import static wrappers.routetable.RouteTableWrapper.table;
 
 public class RouteTableTest {
@@ -64,6 +66,43 @@ public class RouteTableTest {
         route.setAttribute(dummyAttr(1));
 
         assertThat(routeTable.getRoute(destination, outLink), is(dummyRoute(0, 0, path())));
+    }
+
+    @Test
+    public void
+    setRoute_WithAttr2AndEmptyPathForOutLinkAlreadyAssignedWithRouteWithAttr1AndEmpty_GetsRouteWithAttr2AndEmptyPath()
+            throws Exception {
+        RouteTable routeTable = table(dummyOutLink(0, 1));
+        Link outLink = dummyLink(0, 1);
+        Node destination = new Node(0);
+        routeTable.setRoute(outLink, dummyRoute(0, 1, path()));
+
+        routeTable.setRoute(outLink, dummyRoute(0, 2, path()));
+
+        assertThat(routeTable.getRoute(destination, outLink), is(dummyRoute(0, 2, path())));
+    }
+
+    @Test
+    public void
+    getRoute_ForDestination1OnTableWithOnlyKnownDestination0_InvalidRoute() throws Exception {
+        RouteTable routeTable = table(
+                                dummyOutLink(0, 1),
+                destination(0), dummyRoute(0, path())
+        );
+        Node destination1 = new Node(1);
+
+        assertThat(routeTable.getRoute(destination1, dummyLink(0, 1)), is(invalidRoute(destination1)));
+    }
+
+    @Test
+    public void
+    getRoute_ForDestination0AndOutLink1To2ButTheRouteForDestination0WasOnlySetForOutLink1To3_InvalidRoute()
+            throws Exception {
+        RouteTable table = table(dummyOutLink(1, 2), dummyOutLink(1, 3));
+        Node destination = new Node(0);
+        table.setRoute(dummyLink(1, 2), dummyRoute(0, 0, path()));  // assigned attribute to other link
+
+        assertThat(table.getRoute(destination, dummyLink(1, 3)), is(invalidRoute(destination)));
     }
 
 }
