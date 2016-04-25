144358aa4689f0e6e95ef8d1811c4a06c175963e
David Fialho <fialho.david@gmail.com>
2016-04-25 20:56:37 +0100

    implemented the setRoute() and getRoute() methods

--- src/simulation/RouteTable.java
+++ src/simulation/RouteTable.java
@@ -3,44 +3,66 @@ package simulation;
 import dnl.utils.text.table.TextTable;
 import network.Link;
 import network.Node;
-import policies.Attribute;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RouteTable {
 
+    /*
+        The first map represents the out-links. A out-link maps to a map with the destination as the key and the
+        associated route as the value. The out-link must be the initial map keys in order to be possible to create
+        the maps in the constructor.
+     */
+    private Map<Link, Map<Node, Route>> routes;
+
+
     /**
      * Constructs a new empty route table. Defines the outLinks included in the route table.
+     *
      * @param outLinks out out-links of the route table.
      */
     public RouteTable(Collection<Link> outLinks) {
-        // TODO implement
-        throw new UnsupportedOperationException();
+        this.routes = new HashMap<>(outLinks.size());
+
+        // create empty maps for each out-link
+        outLinks.forEach(link -> routes.put(link, new HashMap<>()));
     }
 
     /**
-     * Associates a route with the given out-link. If the out-link does not exist this method
-     * will have no effect on the table. If the destination is unknown it will be added to the table.
+     * Associates a route with the given out-link. If the out-link does not exist this method will have no effect
+     * on the table. If the destination is unknown it will be added to the table.
      *
      * @param outLink out-link to associate route with.
      * @param route route to be set.
      */
     public void setRoute(Link outLink, Route route) {
-        // TODO implement
-        throw new UnsupportedOperationException();
+        Map<Node, Route> destinationToRoute = routes.get(outLink);
+        if (destinationToRoute != null) {
+            // out link does exist
+            destinationToRoute.put(route.getDestination(), new Route(route));
+        }
     }
 
     /**
-     * Returns the route associated with the given destination and out-link pair. If the destination or the
-     * out-link do not exist in the table it will be returned null.
+     * Returns the route associated with the given destination and out-link pair. If the out-link does not exist in
+     * the table it will be returned null.
      *
      * @param destination destination to get route.
      * @param outLink out-link to get route.
-     * @return route associated with the given pair or null if one of them does not exist.
+     * @return route associated with the given pair or null if the out-link does not exist.
      */
-    public Attribute getRoute(Node destination, Link outLink) {
-        // TODO implement
-        throw new UnsupportedOperationException();
+    public Route getRoute(Node destination, Link outLink) {
+        Route route = null;
+
+        Map<Node, Route> destinationToRoute = routes.get(outLink);
+        if (destinationToRoute != null) {
+            // out-link does exist
+            route = destinationToRoute.getOrDefault(destination, Route.invalidRoute(destination));
+        }
+
+        return route;
     }
 
     /**
--- test/unit/simulation/RouteTableTest.java
+++ test/unit/simulation/RouteTableTest.java
@@ -1,5 +1,69 @@
 package simulation;
 
+import network.Link;
+import network.Node;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static simulation.Route.invalidRoute;
+import static wrappers.DummyWrapper.*;
+import static wrappers.PathWrapper.path;
+import static wrappers.network.NetworkWrapper.anyNode;
+import static wrappers.routetable.RouteTableWrapper.table;
+
 public class RouteTableTest {
+
+    @Test
+    public void getRoute_OnTableWithoutOutLinks_Null() throws Exception {
+        assertThat(table().getRoute(anyNode(), anyDummyLink()), is(nullValue()));
+    }
+
+    @Test
+    public void setRoute_WithAttr1AndEmptyPathOnTableWithoutOutLinks_GetsNull() throws Exception {
+        RouteTable routeTable = table();
+        Node destination = new Node(0);
+        Link outLink = dummyLink(1, 2);
+
+        routeTable.setRoute(outLink, dummyRoute(0, 1, path()));
+
+        assertThat(routeTable.getRoute(destination, outLink), is(nullValue()));
+    }
+
+    @Test
+    public void getRoute_ForOutLink1To2OnTableWithoutKnownDestinationsAndOutLinkFrom1To2_Invalid() throws Exception {
+        RouteTable routeTable = table(dummyOutLink(1, 2));
+        Node destination = new Node(0);
+
+        assertThat(routeTable.getRoute(destination, dummyLink(1, 2)), is(invalidRoute(destination)));
+    }
+
+    @Test
+    public void
+    setRoute_FromOutLink1To2AndForDest0WithAttr1AndEmptyPath_GetsRouteWithAttr0AndEmptyPathForDest0() throws Exception {
+        RouteTable routeTable = table(dummyOutLink(1, 2));
+        Node destination = new Node(0);
+        Link outLink = dummyLink(1, 2);
+
+        routeTable.setRoute(outLink, dummyRoute(0, 1, path()));
+
+        assertThat(routeTable.getRoute(destination, outLink), is(dummyRoute(0, 1, path())));
+    }
+
+    @Test
+    public void
+    setRoute_ForDest0WithAttr0AndEmptyPathAndAfterItChangeAttrToAttr1_GetsRouteForDest0WithAttr0AndEmptyPath()
+            throws Exception {
+        Node destination = new Node(0);
+        Link outLink = dummyLink(1, 2);
+        Route route = dummyRoute(0, 0, path());
+        RouteTable routeTable = table(dummyOutLink(1, 2));
+
+        routeTable.setRoute(outLink, route);
+        route.setAttribute(dummyAttr(1));
+
+        assertThat(routeTable.getRoute(destination, outLink), is(dummyRoute(0, 0, path())));
+    }
 
 }
