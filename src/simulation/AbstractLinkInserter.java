4129843958e7cf9c43f5266a0299c702a52199b6
David Fialho <fialho.david@gmail.com>
2016-04-27 12:53:47 +0100

    the insert() method now adds the link's nodes if they don't exist in the network

--- src/simulation/AbstractLinkInserter.java
+++ src/simulation/AbstractLinkInserter.java
@@ -10,13 +10,16 @@ import network.exceptions.NodeNotFoundException;
 public abstract class AbstractLinkInserter implements LinkInserter {
 
     /**
-     * Inserts one link into the given network.
+     * Inserts one link into the given network. If one of the nodes of the link does not exist in the network
+     * the node is added and the link as well.
      *
      * @param link link to be inserted.
      * @param network network to insert link on.
      */
     protected void insert(Link link, Network network) {
         try {
+            network.addNode(link.getSource());
+            network.addNode(link.getDestination());
             network.addLink(link);
         } catch (NodeNotFoundException e) {
             // ignore exception since it is expected to be thrown
