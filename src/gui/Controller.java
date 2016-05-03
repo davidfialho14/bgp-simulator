212cb257e45c0f57eeabf70cfe52bb499568205f
David Fialho <fialho.david@gmail.com>
2016-05-03 23:32:59 +0100

    the start button is now the default and is kept disabled when the network text field is empty resized the main window to be more packed

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,18 +1,32 @@
 package gui;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
 
     public GridPane pane;
     public TextField networkTextField;
+    public Button startButton;
 
     private FileChooser fileChooser = new FileChooser();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        // ensure the start button is disabled when the text field is empty
+        networkTextField.textProperty().addListener((observable, oldText, newText) -> {
+            startButton.setDisable(newText.isEmpty());
+        });
+    }
 
     public void handleClickedBrowseButton(ActionEvent actionEvent) {
         File file = fileChooser.showOpenDialog(pane.getScene().getWindow());
