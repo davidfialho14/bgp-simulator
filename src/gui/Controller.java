0ba00188def3e40b9050b513fea11d033f88ee19
David Fialho <fialho.david@gmail.com>
2016-05-03 23:08:02 +0100

    included a file chooser to select the network file the text field is updated with the absolute path of the network file

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,4 +1,24 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class Controller {
+
+    public GridPane pane;
+    public TextField networkTextField;
+
+    private FileChooser fileChooser = new FileChooser();
+
+    public void handleClickedBrowseButton(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(pane.getScene().getWindow());
+
+        if (file != null && file.exists()) {
+            networkTextField.setText(file.getAbsolutePath());
+        }
+    }
 }
