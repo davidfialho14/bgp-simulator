2d31e7a63879132e5fc2759a70d224c3f4a8c4e0
David Fialho <fialho.david@gmail.com>
2016-05-08 16:42:33 +0100

    removed the EventHandler class

--- src/simulation/implementations/handlers/NullEventHandler.java
+++
@@ -1,9 +0,0 @@
-package simulation.implementations.handlers;
-
-import simulation.EventHandler;
-
-/**
- * Implements an event handler that does nothing.
- */
-public class NullEventHandler extends EventHandler {
-}
--- test/integration/simulation/SimulateEngineTest.java
+++ test/integration/simulation/SimulateEngineTest.java
@@ -1,12 +1,10 @@
 package simulation;
 
 import protocols.Protocol;
-import simulation.implementations.handlers.NullEventHandler;
 
 public class SimulateEngineTest {
 
     protected Engine engine;
-    protected EventHandler eventHandler = new NullEventHandler();
     protected Protocol protocol;
 
 }
