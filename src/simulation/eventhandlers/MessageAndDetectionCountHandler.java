7c615a7a54c1d1a37ab99f6cbee24623f5cd0535
David Fialho <fialho.david@gmail.com>
2016-05-08 20:35:31 +0100

    reimplemented the MessageAndDetectionCountHandler with the new event system

---
+++ src/simulation/eventhandlers/MessageAndDetectionCountHandler.java
@@ -0,0 +1,49 @@
+package simulation.eventhandlers;
+
+import simulation.events.*;
+
+public class MessageAndDetectionCountHandler implements ExportListener, DetectListener {
+
+    private int messageCount = 0;
+    private int detectionCount = 0;
+
+    // --- PUBLIC INTERFACE -----------------------------------------------------------------------------------------
+
+    /**
+     * Returns the current message count.
+     * @return current message count.
+     */
+    public int getMessageCount() {
+        return messageCount;
+    }
+
+    /**
+     * Returns the current detection count.
+     * @return current detection count.
+     */
+    public int getDetectionCount() {
+        return detectionCount;
+    }
+
+    /**
+     * Registers the event handler in the given event generator.
+     *
+     * @param eventGenerator event generator to register on.
+     */
+    public void register(SimulationEventGenerator eventGenerator) {
+        eventGenerator.addExportListener(this);
+        eventGenerator.addDetectListener(this);
+    }
+
+    // --- EVENTS ---------------------------------------------------------------------------------------------------
+
+    @Override
+    public void onExported(ExportEvent event) {
+        messageCount++;
+    }
+
+    @Override
+    public void onDetected(DetectEvent event) {
+        detectionCount++;
+    }
+}
