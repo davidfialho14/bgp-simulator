3854c4446a0561e5f44e2f74ef58fecd9a56d350
David Fialho <fialho.david@gmail.com>
2016-05-08 17:18:45 +0100

    defined the LearnListener interface

---
+++ src/simulation/events/LearnListener.java
@@ -0,0 +1,17 @@
+package simulation.events;
+
+/**
+ * Listener interface for receiving learn events. The class that is interested in processing a learn event
+ * implements this interface, and the object created with that class is registered with a component, using
+ * the component's addLearnListener method. When the learn event occurs, that object's onLearned() method
+ * is invoked.
+ */
+public interface LearnListener {
+
+    /**
+     * Invoked when a learn event occurs.
+     *
+     * @param event learn event that occurred.
+     */
+    void onLearned(LearnEvent event);
+}
