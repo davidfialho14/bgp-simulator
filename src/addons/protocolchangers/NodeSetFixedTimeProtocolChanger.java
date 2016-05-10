3894e99cda193122de5750fe5cf9ca6c50fc5712
David Fialho <fialho.david@gmail.com>
2016-05-10 18:30:44 +0100

    defined and implemented the NodeSetFixedTimeProtocolChanger

---
+++ src/addons/protocolchangers/NodeSetFixedTimeProtocolChanger.java
@@ -0,0 +1,57 @@
+package addons.protocolchangers;
+
+import network.Node;
+import protocols.Protocol;
+import simulation.Engine;
+import simulation.State;
+
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * Fixed time protocol changer based on a set of nodes that are changed when it is time to change the protocol.
+ */
+public class NodeSetFixedTimeProtocolChanger extends FixedTimeProtocolChanger {
+
+    private Protocol protocolToChangeTo;
+    private Set<Node> nodesToChange;
+
+    /**
+     * Creates a fixed time protocol changer with the protocol to change to and the sequence of nodes
+     * to be changed.
+     *
+     * @param changeTime time to change protocol.
+     * @param protocol protocol to change to.
+     * @param nodes nodes to change protocol for.
+     */
+    public NodeSetFixedTimeProtocolChanger(long changeTime, Protocol protocol, Node... nodes) {
+        super(changeTime);
+        this.protocolToChangeTo = protocol;
+        Collections.addAll(nodesToChange, nodes);
+    }
+
+    /**
+     * Creates a fixed time protocol changer by assigning it an engine and a state and associating it
+     * with a time instant to change protocol and with the protocol to change to and the sequence of
+     * nodes to be changed.
+     *
+     * @param engine engine to assign to.
+     * @param state state to assign to.
+     * @param changeTime time to change protocol.
+     * @param protocol protocol to change to.
+     * @param nodes nodes to change protocol for.
+     */
+    public NodeSetFixedTimeProtocolChanger(Engine engine, State state, long changeTime, Protocol protocol, Node... nodes) {
+        super(engine, state, changeTime);
+        this.protocolToChangeTo = protocol;
+        Collections.addAll(nodesToChange, nodes);
+    }
+
+    @Override
+    public void onTimeChange(long newTime) {
+
+        if (isTimeToChange(newTime)) {
+            nodesToChange.forEach(node -> changeProtocol(node, protocolToChangeTo));
+        }
+    }
+}
