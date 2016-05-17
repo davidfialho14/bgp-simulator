87c42415c94e7d38afb1c6551de6f7a151436a9b
David Fialho <fialho.david@gmail.com>
2016-05-17 23:33:11 +0100

    the detecting spinner now is able to take values between 0 and MAX_VALUE

--- src/gui/partialdeployment/PartialDeploymentController.java
+++ src/gui/partialdeployment/PartialDeploymentController.java
@@ -1,13 +1,25 @@
 package gui.partialdeployment;
 
+import gui.basics.NumberSpinner;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Spinner;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.GridPane;
 
-public class PartialDeploymentController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PartialDeploymentController implements Initializable {
 
     public GridPane statsPane;
     public ToggleButton activateToggle;
+    public Spinner<Integer> detectingTimeSpinner;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        NumberSpinner.setupNumberSpinner(detectingTimeSpinner, 0, Integer.MAX_VALUE, 0);
+    }
 
     public void enablePartialDeployment(ActionEvent actionEvent) {
 
