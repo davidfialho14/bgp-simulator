65298747e6cc339595b07130688f9024836cd00d
David Fialho <fialho.david@gmail.com>
2016-05-25 14:52:00 +0100

    added new events: StartEvent and EndEvent

---
+++ src/simulation/events/EndEvent.java
@@ -0,0 +1,13 @@
+package simulation.events;
+
+/**
+ * Events generated when the simulation ends.
+ */
+public class EndEvent implements SimulationEvent {
+
+    @Override
+    public String toString() {
+        return "EndEvent{}";
+    }
+
+}
