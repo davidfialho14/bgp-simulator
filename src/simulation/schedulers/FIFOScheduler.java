6e6d16519e4a91ae0095ce4617893641af40e28a
David Fialho <fialho.david@gmail.com>
2016-05-25 09:52:05 +0100

    added to the schedulers the possibility to set the minimum and maximum message delays

--- src/simulation/schedulers/FIFOScheduler.java
+++ src/simulation/schedulers/FIFOScheduler.java
@@ -7,4 +7,23 @@ public class FIFOScheduler extends AbstractScheduler {
         return scheduledRoute.getTimestamp() + 1;
     }
 
+    /**
+     * Sets the minimum message delay. (implementation is optional)
+     *
+     * @param delay delay to set as minimum.
+     */
+    @Override
+    public void setMinDelay(int delay) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Sets the maximum message delay. (implementation is optional)
+     *
+     * @param delay delay to set as maximum.
+     */
+    @Override
+    public void setMaxDelay(int delay) {
+        throw new UnsupportedOperationException();
+    }
 }
