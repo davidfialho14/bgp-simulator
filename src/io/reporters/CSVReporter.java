a277a19831c6c88c29f2ebb4ba65fe28d243862b
David Fialho <fialho.david@gmail.com>
2016-05-25 16:36:11 +0100

    made the CSV reports a little bit prettier

--- src/io/reporters/CSVReporter.java
+++ src/io/reporters/CSVReporter.java
@@ -1,11 +1,17 @@
 package io.reporters;
 
+import network.Link;
+import network.Node;
+import org.apache.commons.lang.StringUtils;
+import policies.Path;
 import simulators.statscollectors.*;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Generates reports in CSV format.
@@ -42,8 +48,10 @@ public class CSVReporter extends Reporter {
                 report.write("; DETECTING NODE; CUT-OFF LINK; CYCLE"); report.newLine();
 
                 for (Detection detection : statsCollector.getDetections(i)) {
-                    report.write("; " + detection.getDetectingNode() + "; " + detection.getCutoffLink() + "; " +
-                            detection.getCycle());
+                    report.write("; " +
+                            pretty(detection.getDetectingNode()) + "; " +
+                            pretty(detection.getCutoffLink()) + "; " +
+                            pretty(detection.getCycle()));
                     report.newLine();
                 }
             }
@@ -73,8 +81,10 @@ public class CSVReporter extends Reporter {
                 report.write("; DETECTING NODE; CUT-OFF LINK; CYCLE"); report.newLine();
 
                 for (Detection detection : statsCollector.getDetections(i)) {
-                    report.write("; " + detection.getDetectingNode() + "; " + detection.getCutoffLink() + "; " +
-                            detection.getCycle());
+                    report.write("; " +
+                            pretty(detection.getDetectingNode()) + "; " +
+                            pretty(detection.getCutoffLink()) + "; " +
+                            pretty(detection.getCycle()));
                     report.newLine();
                 }
             }
@@ -103,8 +113,11 @@ public class CSVReporter extends Reporter {
                 report.write("; DETECTING NODE; CUT-OFF LINK; CYCLE; FALSE POSITIVE"); report.newLine();
 
                 for (Detection detection : statsCollector.getDetections(i)) {
-                    report.write("; " + detection.getDetectingNode() + "; " + detection.getCutoffLink() + "; " +
-                            detection.getCycle() + "; " + detection.isFalsePositive());
+                    report.write("; " +
+                            pretty(detection.getDetectingNode()) + "; " +
+                            pretty(detection.getCutoffLink()) + "; " +
+                            pretty(detection.getCycle()) + "; " +
+                            (detection.isFalsePositive() ? "Yes" : "No"));
                     report.newLine();
                 }
             }
@@ -127,6 +140,7 @@ public class CSVReporter extends Reporter {
                 report.write("; Detecting nodes count:; " + statsCollector.getDetectingNodesCount(i)); report.newLine();
                 report.write("; Cut-off links count:; " + statsCollector.getCutOffLinksCount(i)); report.newLine();
                 report.write("; Message Count After Deployment:; " + statsCollector.getMessageCountAfterDeployment(i));
+                report.newLine();
                 report.write("; False positive count:; " + statsCollector.getFalsePositiveCount(i)); report.newLine();
                 report.newLine();
 
@@ -134,11 +148,35 @@ public class CSVReporter extends Reporter {
                 report.write("; DETECTING NODE; CUT-OFF LINK; CYCLE; FALSE POSITIVE"); report.newLine();
 
                 for (Detection detection : statsCollector.getDetections(i)) {
-                    report.write("; " + detection.getDetectingNode() + "; " + detection.getCutoffLink() + "; " +
-                            detection.getCycle() + "; " + detection.isFalsePositive());
+                    report.write("; " +
+                            pretty(detection.getDetectingNode()) + "; " +
+                            pretty(detection.getCutoffLink()) + "; " +
+                            pretty(detection.getCycle()) + "; " +
+                            (detection.isFalsePositive() ? "Yes" : "No"));
                     report.newLine();
                 }
             }
         }
+    }
+
+    /*
+        Set of helper method that allow displaying any element like Nodes, Links, Paths, etc into a more prettier
+        format.
+     */
+
+    private static String pretty(Node node) {
+        return String.valueOf(node.getId());
+    }
+
+    private static String pretty(Link link) {
+        return link.getSource().getId() + " → " + link.getDestination().getId();
+    }
+
+    private static String pretty(Path path) {
+        List<Integer> pathNodesIds = path.stream()
+                .map(Node::getId)
+                .collect(Collectors.toList());
+
+        return StringUtils.join(pathNodesIds.iterator(), " → ");
     }
 }
