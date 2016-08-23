388ed359217890798fd48197b0b84b84015cb9db
david <fialho.david@gmail.com>
2016-08-23 18:26:12 +0100

    added documentation for the simulator factories

--- src/simulators/SimulatorFactory.java
+++ src/simulators/SimulatorFactory.java
@@ -15,9 +15,13 @@ public interface SimulatorFactory {
      * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
     /**
-     * @param engine
-     * @param topology
-     * @param destinationId
+     * Creates a new simulator instance with the pre-configured parameters. The returned simulator implementation
+     * depends on the factory implementation and on the parameters provided to the factory.
+     *
+     * @param engine        engine used to simulate.
+     * @param topology      topology to be simulated.
+     * @param destinationId destination node to simulate for.
+     * @return new simulator instance.
      */
     Simulator getSimulator(Engine engine, Topology topology, int destinationId);
 
