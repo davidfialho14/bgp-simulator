65c2f41c993608c37007dfabf5d6ca4d10db9738
david <fialho.david@gmail.com>
2016-08-23 18:15:06 +0100

    the main method now calls the CLI application if there is command line arguments and the GUI application if not

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,21 +1,11 @@
 package main;
 
-import core.Protocol;
-import io.networkreaders.GraphvizReaderFactory;
 import io.networkreaders.PolicyTagger;
-import io.networkreaders.exceptions.TopologyParseException;
-import io.reporters.CSVReporterFactory;
-import javafx.scene.control.Alert;
-import main.gui.SimulatorApplication;
-import org.apache.commons.cli.*;
+import main.cli.CLIApplication;
+import main.gui.GUIApplication;
 import policies.gaorexford.GaoRexfordPolicy;
 import policies.roc.RoCPolicy;
 import policies.shortestpath.ShortestPathPolicy;
-import protocols.D1R1Protocol;
-import protocols.D2R1Protocol;
-
-import java.io.File;
-import java.io.IOException;
 
 public class Main {
 
@@ -25,102 +15,11 @@ public class Main {
         PolicyTagger.register(new RoCPolicy(), "RoC");
         PolicyTagger.register(new GaoRexfordPolicy(), "GaoRexford");
 
-        CommandLineParser parser = new DefaultParser();
-
-        Options options = new Options();
-        options.addOption("d", "destination", true, "simulate with the given destination id");
-        options.addOption("n", "initialTopology", true, "core.initialTopology to be simulated");
-        options.addOption("c", "repetition_count", true, "number of repetitions");
-        options.addOption("d2", "use detection 2 instead of detection 1");
-        options.addOption("deploy", true, "time deploy detection");
-
-        // parse the command line arguments
-        try {
-            CommandLine cmd = parser.parse(options, args);
-
-            File networkFile = null;
-            if (cmd.hasOption("initialTopology")) {
-                networkFile = new File(cmd.getOptionValue("initialTopology"));
-
-                if (!networkFile.exists()) {
-                    System.err.println("Network file does not exist");
-                    System.exit(1);
-                }
-            }
-
-            if (cmd.hasOption("destination")) {
-                // execute directly
-                int destinationId = Integer.parseInt(cmd.getOptionValue("destination"));
-                int repetitionCount = Integer.parseInt(cmd.getOptionValue("repetition_count"));
-
-                if (networkFile == null) {
-                    System.err.println("It is missing the core.initialTopology file");
-                    System.exit(1);
-                }
-
-                Protocol protocol;
-                if (cmd.hasOption("d2")) {
-                    protocol = new D2R1Protocol();
-                } else {
-                    protocol = new D1R1Protocol();
-                }
-
-                Integer deployTime = null;
-                if (cmd.hasOption("deploy")) {
-                    deployTime = Integer.parseInt(cmd.getOptionValue("deploy"));
-                }
-
-                simulate(networkFile, destinationId, repetitionCount, protocol, deployTime);
-
-            } else {
-                // display the GUI
-                SimulatorApplication.launch(args, networkFile);
-            }
-
-
-        } catch (ParseException e) {
-            System.out.println("Unexpected exception:" + e.getMessage());
+        if (args.length == 0) {
+            GUIApplication.launch(args);
+        } else {
+            CLIApplication.launch(args);
         }
-
-    }
-
-    private static void simulate(File topologyFile, int destinationId, int repetitionCount, Protocol protocol,
-                                 Integer deployTime) {
-
-        SimulatorLauncher simulatorLauncher = new SimulatorLauncher(new ErrorHandler() {
-            @Override
-            public void onTopologyLoadIOException(IOException exception) {
-                System.err.println("can not open the file");
-            }
-
-            @Override
-            public void onTopologyLoadParseException(TopologyParseException exception) {
-                System.err.println("initialTopology file is corrupted");
-            }
-
-            @Override
-            public void onReportingIOException(IOException exception) {
-                Alert alert = new Alert(Alert.AlertType.ERROR, "failed to open/create/write report file");
-                alert.setHeaderText("IO Error");
-                alert.showAndWait();
-            }
-        });
-
-        simulatorLauncher.configure()
-                .readerFactory(new GraphvizReaderFactory())
-                .destinationId(destinationId)
-                .repetitionCount(repetitionCount)
-                .protocol(protocol)
-                .reporterFactory(new CSVReporterFactory())
-                .enableFullDeployment(deployTime != null)
-                    .deployTime(deployTime == null ? 0 : deployTime)
-                .commit();
-
-        String reportFileName = topologyFile.getName().replaceFirst("\\.gv",
-                String.format("-dest%02d.csv", destinationId));
-        File reportFile = new File(topologyFile.getParent(), reportFileName);
-
-        simulatorLauncher.launch(topologyFile, reportFile);
 
     }
 
