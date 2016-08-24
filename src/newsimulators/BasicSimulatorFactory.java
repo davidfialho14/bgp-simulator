def149366846562402dd76f965bd16103bedc930
david <fialho.david@gmail.com>
2016-08-24 15:28:50 +0100

    added new simulator factories (equal to the previous ones)

---
+++ src/newsimulators/BasicSimulatorFactory.java
@@ -0,0 +1,31 @@
+package newsimulators;
+
+import core.Engine;
+import core.Protocol;
+import core.State;
+import core.topology.Topology;
+
+/**
+ * Creates instances of basic simulator for the configurations given in the factory constructor.
+ */
+public class BasicSimulatorFactory implements SimulatorFactory {
+
+    private final Protocol protocol;
+
+    public BasicSimulatorFactory(Protocol protocol) {
+        this.protocol = protocol;
+    }
+
+    /**
+     * Creates a new instance of basic simulator.
+     *
+     * @param engine        engine used to simulate.
+     * @param topology      topology to be simulated.
+     * @param destinationId destination node to simulate for.
+     * @return new instance of basic simulator.
+     */
+    @Override
+    public Simulator getSimulator(Engine engine, Topology topology, int destinationId) {
+        return new BasicSimulator(engine, State.create(topology, destinationId, protocol));
+    }
+}
---
+++ src/newsimulators/SimulatorFactory.java
@@ -0,0 +1,22 @@
+package newsimulators;
+
+import core.Engine;
+import core.topology.Topology;
+
+/**
+ * Simulator factory creates instances of simulator classes depending on the given configurations.
+ */
+public interface SimulatorFactory {
+
+    /**
+     * Creates a new simulator instance with the pre-configured parameters. The returned simulator implementation
+     * depends on the factory implementation and on the parameters provided to the factory.
+     *
+     * @param engine        engine used to simulate.
+     * @param topology      topology to be simulated.
+     * @param destinationId destination node to simulate for.
+     * @return new simulator instance.
+     */
+    Simulator getSimulator(Engine engine, Topology topology, int destinationId);
+
+}
