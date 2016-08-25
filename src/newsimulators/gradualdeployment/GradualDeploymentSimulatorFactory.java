56fcfbf1a63b014cd5350b9cefb0195a71091746
david <fialho.david@gmail.com>
2016-08-25 21:30:58 +0100

    implemented the gradual deployment simulator

---
+++ src/newsimulators/gradualdeployment/GradualDeploymentSimulatorFactory.java
@@ -0,0 +1,51 @@
+package newsimulators.gradualdeployment;
+
+import core.Engine;
+import core.Protocol;
+import core.State;
+import core.topology.Topology;
+import newsimulators.Simulator;
+import newsimulators.SimulatorFactory;
+import protocols.BGPProtocol;
+
+/**
+ * Creates instances of gradual deployment simulator with the configurations given in the factory constructor.
+ */
+public class GradualDeploymentSimulatorFactory implements SimulatorFactory {
+
+    private final Protocol deployProtocol;
+    private final int deployPeriod;
+    private final int deployPercentage;
+
+    /**
+     * Creates a new factory instance. It takes as arguments the specific parameters for this type of simulators.
+     * The returned simulator implementation does not depend on this parameters.
+     *
+     * @param deployProtocol    deployProtocol to deploy gradually.
+     * @param deployPeriod      period to have new nodes deploying a new protocol.
+     * @param deployPercentage  percentage of network nodes to deploy new protocol each period.
+     */
+    public GradualDeploymentSimulatorFactory(Protocol deployProtocol, int deployPeriod, int deployPercentage) {
+        this.deployProtocol = deployProtocol;
+        this.deployPeriod = deployPeriod;
+        this.deployPercentage = deployPercentage;
+    }
+
+    /**
+     * Creates a new instance of gradual deployment simulator with the pre-configured parameters.
+     *
+     * @param engine        engine used to simulate.
+     * @param topology      topology to be simulated.
+     * @param destinationId destination node to simulate for.
+     * @return new instance of basic simulator.
+     */
+    @Override
+    public Simulator getSimulator(Engine engine, Topology topology, int destinationId) {
+        // always initiate with the BGP deployProtocol!
+        State initialState = State.create(topology, destinationId, new BGPProtocol());
+        int deployedNodeCount = (int) Math.ceil(deployPercentage * topology.getNetwork().getNodeCount());
+
+        return new GradualDeploymentSimulator(engine, initialState, deployProtocol, deployPeriod, deployedNodeCount);
+    }
+
+}
