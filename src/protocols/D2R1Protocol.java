5d67231ebc4bc9bf72f21a2147924fa1ba543b11
David Fialho <fialho.david@gmail.com>
2016-10-13 17:47:12 +0100

    reimplemented D2R1Protocol to take advantage of the new divided Detection and Reaction implementations

--- src/protocols/D2R1Protocol.java
+++ src/protocols/D2R1Protocol.java
@@ -1,40 +1,15 @@
 package protocols;
 
-import core.Attribute;
-import core.Protocol;
-import core.Route;
-import core.topology.Link;
-
 /**
- * Implements the detection D2 and the reaction R1.
+ * Its just a wrapper around a generic protocol implementing the Path Detection and the Cut-Off Reaction.
  */
-public class D2R1Protocol implements Protocol {
+public class D2R1Protocol extends GenericProtocol {
 
-    private final Reaction1 reaction = new Reaction1();
-
-    @Override
-    public boolean isPolicyDispute(Link link, Route learnedRoute, Route alternativeRoute) {
-        return Detections.detection2(link.getSource(), learnedRoute, alternativeRoute);
-    }
-
-    @Override
-    public void detectionInfo(Link link, Route learnedRoute, Route alternativeRoute) {
-        reaction.setParameters(link, learnedRoute);
-    }
-
-    @Override
-    public Attribute extend(Attribute attribute, Link link) {
-        return reaction.extend(attribute, link);
-    }
-
-    @Override
-    public void reset() {
-        reaction.reset();
-    }
-
-    @Override
-    public String toString() {
-        return "D2R1";
+    /**
+     * Initializes the protocol given the path detection and cut-off reaction implementations.
+     */
+    public D2R1Protocol() {
+        super(new PathDetection(), new CutOffReaction());
     }
 
 }
