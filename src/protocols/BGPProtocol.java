6b51fc03dd5031c07d084d5d28d6c15bf3b0425e
David Fialho <fialho.david@gmail.com>
2016-10-13 12:53:48 +0100

    added some documentation to the BGP protocol

--- src/protocols/BGPProtocol.java
+++ src/protocols/BGPProtocol.java
@@ -6,23 +6,46 @@ import core.Route;
 import core.topology.Link;
 import core.topology.Node;
 
+/**
+ * Implements the common BGP protocol. This means that the extend operation will be the common BGP operation without
+ * any changes and any other operation will be ignored or do absolutely nothing.
+ */
 public class BGPProtocol implements Protocol {
 
+    /**
+     * Performs the common BGP extend. This means it simply extends the attribute using the link extend operation.
+     *
+     * @param destination the destination node.
+     * @param link link to extend the attribute.
+     * @param attribute attribute to be extended.
+     * @return the extended attribute.
+     */
     @Override
     public Attribute extend(Node destination, Link link, Attribute attribute) {
         return link.extend(attribute);
     }
 
+    /**
+     * Always returns false since the protocol does not detect oscillations.
+     *
+     * @return false
+     */
     @Override
     public boolean isOscillation(Link link, Route learnedRoute, Route exclRoute) {
         return false;
     }
 
+    /**
+     * Does nothing.
+     */
     @Override
     public void setParameters(Link link, Route learnedRoute, Route exclRoute) {
         // ignore
     }
 
+    /**
+     * Does nothing.
+     */
     @Override
     public void reset() {
 
