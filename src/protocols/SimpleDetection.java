890e9d4b8f4eaf5e5c67c041e69812ab7db562bf
David Fialho <fialho.david@gmail.com>
2016-10-13 16:35:47 +0100

    implemented the Simple detection

---
+++ src/protocols/SimpleDetection.java
@@ -0,0 +1,25 @@
+package protocols;
+
+import core.Route;
+import core.topology.Link;
+
+/**
+ * Implementation of the simple detection which requires only one conditions:
+ *  - the attribute of the learned route must be preferred to the attribute of the alternative route
+ */
+public class SimpleDetection implements Detection {
+
+    /**
+     * Checks if the attribute of the learned route is preferred to the attribute of the alternative route.
+     *
+     * @param link             ignored
+     * @param learnedRoute     new learned route.
+     * @param alternativeRoute most preferred route learned from other neighbor (not the destination node of the link)
+     * @return true if the attribute of the learned route is preferred to the attribute of the alternative route.
+     */
+    @Override
+    public boolean isPolicyDispute(Link link, Route learnedRoute, Route alternativeRoute) {
+        return learnedRoute.getAttribute().compareTo(alternativeRoute.getAttribute()) < 0;
+    }
+
+}
