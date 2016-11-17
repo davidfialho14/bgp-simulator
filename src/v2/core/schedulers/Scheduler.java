b1cc21d854919ca3fce6c713d9fd8b69a711e85e
David Fialho <fialho.david@gmail.com>
2016-11-17 22:17:21 +0000

    Defined the "Scheduler" interface

---
+++ src/v2/core/schedulers/Scheduler.java
@@ -0,0 +1,41 @@
+package v2.core.schedulers;
+
+
+import v2.core.Message;
+
+public interface Scheduler {
+
+    /**
+     * Adds a new message to the scheduler. The message should contain the current time and the scheduler
+     * might add extra time to the message. The scheduler will always take into account the time associated
+     * with the message's link, which means that the order of the messages at the receiving end will be
+     * the same as at the sending point.
+     *
+     * @param message   message to schedule.
+     */
+    void schedule(Message message);
+
+    /**
+     * Returns the next message in the scheduler. If the scheduler contains no more message then null is
+     * returned.
+     *
+     * @return the next message in the scheduler or null if there is no messages.
+     */
+    Message nextMessage();
+
+    /**
+     * Checks if the scheduler has any messages left.
+     *
+     * @return true if the scheduler contains messages or false if the scheduler is empty.
+     */
+    boolean hasMessages();
+
+    /**
+     * Returns the current scheduler time. The scheduler's time is given by the time of the next message in
+     * the scheduler.
+     *
+     * @return the current time of the scheduler.
+     */
+    int getTime();
+
+}
