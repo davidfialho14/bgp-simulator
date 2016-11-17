9601fa0b7ad6612085a8ec69fa7e90e3aa2220b7
David Fialho <fialho.david@gmail.com>
2016-11-17 21:08:14 +0000

    Removed the reset() method from the "Protocol" interface and the router argument from the process() method

--- src/v2/core/Protocol.java
+++ src/v2/core/Protocol.java
@@ -2,23 +2,15 @@ package v2.core;
 
 /**
  * Base interface for any protocol implementation. All protocol implementations must implement this interface.
- * Protocols might have state and therefore must be unique for each router and need to be reset before each
- * simulation.
  */
 public interface Protocol {
 
     /**
      * Processes a message that arrives at the given router.
      *
-     * @param router    router receiving the message.
      * @param message   message to process.
      * @param exporter  exporter used to export a route.
      */
-    void process(Router router, Message message, Exporter exporter);
-
-    /**
-     * Resets the state of the protocol. Protocols must be reset before each simulation.
-     */
-    void reset();
+    void process(Message message, Exporter exporter);
 
 }
