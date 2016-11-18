3777b03ecb8a6c96a7a047feade0a4b784f83f84
David Fialho <fialho.david@gmail.com>
2016-11-18 15:46:45 +0000

    Added the "Shortest Path Policy"

---
+++ test/unit/v2/core/policies/shortestpath/ShortestPathAttributeTest.java
@@ -0,0 +1,28 @@
+package v2.core.policies.shortestpath;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static v2.core.InvalidAttribute.invalidAttr;
+import static v2.wrappers.ShortestPathWrapper.spAttr;
+
+public class ShortestPathAttributeTest {
+
+    @Test
+    public void compareTo_AttributeWithLength0WithInvalidAttribute_Less() throws Exception {
+        assertThat(spAttr(0).compareTo(invalidAttr()), lessThan(0));
+    }
+
+    @Test
+    public void compareTo_AttributeWithLength1WithAttributeWithLength2_Less() throws Exception {
+        assertThat(spAttr(1).compareTo(spAttr(2)), lessThan(0));
+    }
+
+    @Test
+    public void compareTo_AttributeWithLength2WithAttributeWithLength1_Greater() throws Exception {
+        assertThat(spAttr(2).compareTo(spAttr(1)), greaterThan(0));
+    }
+
+}
