c91e95d42f46cf570557322f18fcf0589294ceb1
David Fialho <fialho.david@gmail.com>
2016-11-18 17:42:38 +0000

    Added documentation to "Simulator"

--- src/v2/simulators/Simulator.java
+++ src/v2/simulators/Simulator.java
@@ -4,28 +4,56 @@ import v2.io.reporters.Reporter;
 
 import java.io.IOException;
 
+/**
+ * A simulator is used to do the actual simulation. It is a template class that takes a simulation setup
+ * and executes teh necessary steps to start a simulation: setup, start, and cleanup.
+ */
 public class Simulator {
 
-    private Setup setup;
+    private Setup setup;    // setup of the simulation
 
+    /**
+     * Creates a simulator and assigns it an initial setup.
+     *
+     * @param setup setup to assign to the new simulator.
+     */
     public Simulator(Setup setup) {
         this.setup = setup;
     }
 
+    /**
+     * Returns the simulation setup.
+     *
+     * @return the simulation setup.
+     */
     public Setup getSetup() {
         return setup;
     }
 
+    /**
+     * Sets a simulation setup for the simulator.
+     *
+     * @param setup setup to set.
+     */
     public void setSetup(Setup setup) {
         this.setup = setup;
     }
 
+    /**
+     * Entry point to start the simulations.
+     */
     public void simulate() {
         setup.setup();
         setup.start();
         setup.cleanup();
     }
 
+    /**
+     * Reports the results of the last simulation.
+     *
+     * @param reporter  reporter implementation to report data.
+     * @throws IOException if there is an IO error while reporting.
+     */
     public void report(Reporter reporter) throws IOException {
         setup.getDataCollector().report(reporter);
     }
