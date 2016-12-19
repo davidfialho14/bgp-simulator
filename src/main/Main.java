8f6eb6cdd87ff78cd0506e52d8085cb6dd821efe
David Fialho <fialho.david@gmail.com>
2016-12-19 13:35:25 +0000

    Added CLI option to display the simulator's version

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,14 @@ import main.cli.CLIApplication;
 
 public class Main {
 
+    private static final String VERSION = "2.1.0";
+
     public static void main(String[] args) {
+
+        if (args.length == 1 && args[0].equals("-v")) {
+            System.out.println("SS-BGP Simulator: v" + VERSION);
+            System.exit(1);
+        }
 
         PolicyTagger.register(new ShortestPathPolicy(), "ShortestPath");
         PolicyTagger.register(new PeerPlusPolicy(), "RoC");
