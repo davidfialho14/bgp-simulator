eaea5e72bede38ab28cc6f71fb1223c4f499fac0
David Fialho <fialho.david@gmail.com>
2017-01-12 23:37:16 +0000

    Implemented a new peer+ label based on the GR label

---
+++ src/core/policies/peerplus/PeerPlusLab.java
@@ -0,0 +1,56 @@
+package core.policies.peerplus;
+
+import core.Attribute;
+import core.Label;
+import core.Link;
+import core.policies.peerplus.PeerPlusAttr.*;
+
+import java.util.EnumMap;
+
+import static core.InvalidAttribute.invalidAttr;
+import static core.policies.peerplus.PeerPlusAttr.*;
+
+public enum PeerPlusLab implements Label {
+
+    PeerPlus(peerplus(), peerplus(), peerplus(), invalidAttr(), invalidAttr()),
+    Customer(customer(), customer(), customer(), invalidAttr(), invalidAttr()),
+    Peer(peer(), peer(), peer(), invalidAttr(), invalidAttr()),
+    Provider(provider(), provider(), provider(), provider(), provider());
+
+    private final EnumMap<Value, Attribute> map = new EnumMap<>(Value.class);
+
+    PeerPlusLab(Attribute selfMapValue, Attribute peerplusMapValue, Attribute customerMapValue,
+                Attribute peerMapValue, Attribute providerValue) {
+
+        map.put(Value.Self, selfMapValue);
+        map.put(Value.Customer, customerMapValue);
+        map.put(Value.Peer, peerMapValue);
+        map.put(Value.Provider, providerValue);
+        map.put(Value.Provider, providerValue);
+    }
+
+    public static Label peerplusLabel() {
+        return PeerPlus;
+    }
+
+    public static Label customerLabel() {
+        return Customer;
+    }
+
+    public static Label peerLabel() {
+        return Peer;
+    }
+
+    public static Label providerLabel() {
+        return Provider;
+    }
+
+    @Override
+    public Attribute extend(Link link, Attribute attribute) {
+        if (attribute == invalidAttr()) return invalidAttr();
+
+        PeerPlusAttr peerplusAttribute = (PeerPlusAttr) attribute;
+        return map.get(peerplusAttribute.value);
+    }
+
+}
